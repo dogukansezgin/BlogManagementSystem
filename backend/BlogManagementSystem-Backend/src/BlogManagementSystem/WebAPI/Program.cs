8c7cdb5e38c6d83ed0ad5dfc335ca56dabc087d3
Doğukan Sezgin <dogukan_sezgin@hotmail.com>
2024-07-14 04:30:31 +0300

    backend: Controllers added and program settings configured.

--- backend/BlogManagementSystem-Backend/src/BlogManagementSystem/WebAPI/Program.cs
+++ backend/BlogManagementSystem-Backend/src/BlogManagementSystem/WebAPI/Program.cs
@@ -1,3 +1,15 @@
+using Application;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using NArchitecture.Core.CrossCuttingConcerns.Exception.WebApi.Extensions;
+using NArchitecture.Core.Localization.WebApi;
+using NArchitecture.Core.Security.DependencyInjection;
+using NArchitecture.Core.Security.Encryption;
+using NArchitecture.Core.Security.JWT;
+using NArchitecture.Core.Security.WebApi.Swagger.Extensions;
+using Persistence;
+using Swashbuckle.AspNetCore.SwaggerUI;
 using WebAPI;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,12 +18,78 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddApplicationServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
+
+const string tokenOptionsConfigurationSection = "TokenOptions";
+TokenOptions tokenOptions =
+    builder.Configuration.GetSection(tokenOptionsConfigurationSection).Get<TokenOptions>()
+    ?? throw new InvalidOperationException($"\"{tokenOptionsConfigurationSection}\" section cannot found in configuration.");
+
+builder.Services.AddSecurityServices<Guid, int, Guid>(tokenOptions);
+
+builder
+    .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = tokenOptions.Issuer,
+            ValidAudience = tokenOptions.Audience,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+        };
+    });
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.AddSecurityDefinition(
+        name: "Bearer",
+        securityScheme: new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description =
+                "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer YOUR_TOKEN\". \r\n\r\n"
+                + "`Enter your token in the text input below.`"
+        }
+    );
+    opt.OperationFilter<BearerSecurityRequirementOperationFilter>();
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(opt =>
+    {
+        opt.DocExpansion(DocExpansion.None);
+    });
+}
 
-app.UseHttpsRedirection();
+if (app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
+    app.ConfigureCustomExceptionMiddleware();
+}
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -21,5 +99,7 @@ WebApiConfiguration webApiConfiguration =
     app.Configuration.GetSection(webApiConfigurationSection).Get<WebApiConfiguration>()
     ?? throw new InvalidOperationException($"\"{webApiConfigurationSection}\" section cannot found in configuration.");
 app.UseCors(opt => opt.WithOrigins(webApiConfiguration.AllowedOrigins).AllowAnyHeader().AllowAnyMethod().AllowCredentials());
+
+app.UseResponseLocalization();
 
 app.Run();
